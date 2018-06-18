465c89670c55d1d23701093f4a4a8eada1b062c7
JeffChau0503 <Jeff_Chau0503@hotmail.com>
2018-06-18 12:46:53 +0200

    MA2

--- preprocessing.py
+++ preprocessing.py
@@ -1,9 +1,12 @@
 # encoding=utf-8
+# In[1]:
 import numpy as np
 import matplotlib.pyplot as plt
+import scipy.misc
+import os
 
 """
-数据路径
+数据路径:
 E:/Datafile/PyriteRefl0614/DataPR0614.txt
 
 Reflectance mode with 9 Pixels:
@@ -17,36 +20,33 @@ E:/Datafile/ZielAbso0614/DataGA.txt
 E:/Datafile/ZielAbso0614/DataPA.txt
 """
 
-data = np.loadtxt('E:/Datafile/ZielAbso0614/DataPA.txt')  # file为二维数组. 代码含义:把这些数据读取出来,存为numpy数组.该方法只适用于txt文件中是纯数字(float,int)
+data_path = 'E:/Datafile/PyriteRefl0614/DataPR0614.txt'
+# data为二维数组. 代码含义:把这些数据读取出来,存为numpy数组.该方法只适用于txt文件中是纯数字(float,int)
+data = np.loadtxt(data_path)
+
+
 m = data.shape[0]  # 获取样本数(行数).  shape = file.shape
 n = data.shape[1]  # 获取特征数(列数).
 print("shape of Data: " + str(data.shape))
 print("number of examples: " + str(m))
 
-"""
-def lbl_norm(data):
-    
-    #  Layer by layer normalization 逐层归一化
-    #  :param data:数据data为一个2维数组, 横向为光谱方向, 纵向为空间方向
-    #  :return:归一化后的数据.
-    
+type(data)
+print(data.max(), ' ', data.min())
 
-    layermax = data.max(axis=0)
-    layermin = data.min(axis=0)
-    norm_x = (data - layermin)/(layermax - layermin)
-    print(norm_x.shape)
-    return norm_x
+# build dir for save pics
+type_dir = 'E:/imageset/Type1'
+if os.path.exists(type_dir) == False:
+    os.makedirs(type_dir)
 
-print(lbl_norm(data))
-"""
+for i in range(m):
+    norm = (data[i, :] - data[i, :].min())/(data[i, :].max()-data[i, :].min())
+    norm = 255 * norm
+    norm = norm.astype(np.int32)
+    r = int(np.sqrt(n))
+    matrix = np.resize(norm, (r, r))
+    save_path = os.path.join(type_dir, str(i+1)+'.jpg')
+    scipy.misc.imsave(save_path, matrix)
 
-for pixel in data:  # in的后面接data 或者 归一化后的 def lbl_norm(data)
-    """
-    对每一个像元提取光谱数据,reshape成二维矩阵,imwirte成灰度图片
-    """
-    r = int(np.sqrt(n))  # 确定二维矩阵的边长,开方取整
-    matrix = np.resize(pixel, (r, r))  # reshape成二维矩阵
-    #  print(matrix)
-    plt.imshow(matrix, cmap='Greys_r')
-    plt.show()
+print("End\n")
+
 
