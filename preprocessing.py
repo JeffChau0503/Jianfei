25cd310fbef0ff91b60ad7bd9556184ac5d53846
JeffChau0503 <Jeff_Chau0503@hotmail.com>
2018-07-10 12:24:53 +0200

    MA2

--- preprocessing.py
+++ preprocessing.py
@@ -4,7 +4,7 @@ import numpy as np
 import scipy.misc
 import os
 
-data_path = 'E:/Datafile/P0629.txt'
+data_path = 'E:/Datafile/P0629.txt'  # 更改1 文件读取路径
 # data为二维数组. 代码含义:把这些数据读取出来,存为numpy数组.该方法只适用于txt文件中是纯数字(float,int)
 data = np.loadtxt(data_path)
 
@@ -18,7 +18,7 @@ type(data)
 print("Maximum and minimum: " + str(data.max()), ' ', str(data.min()))
 
 # build dir for save pics
-type_dir = 'E:/imageset/Pyrite0629'
+type_dir = 'E:/imageset/test0710'  # 更改2 文件保存路径
 if os.path.exists(type_dir) == False:
     os.makedirs(type_dir)
 
@@ -28,7 +28,7 @@ for i in range(m):
     norm = norm.astype(np.int32)
     r = int(np.sqrt(n))
     matrix = np.resize(norm, (r, r))
-    save_path = os.path.join(type_dir, str(i+1)+'.jpg')
+    save_path = os.path.join(type_dir, "0_" + str(i+1).zfill(5) + '.jpg')  # 更改3 文件名命名 下划线前表示标签
     scipy.misc.imsave(save_path, matrix)
 
 print("End\n")
