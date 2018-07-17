f97429410c619a6accda8b2eb2667c01ddef1997
JeffChau0503 <Jeff_Chau0503@hotmail.com>
2018-07-17 17:57:59 +0200

    MA2

--- prep_improved.py
+++ prep_improved.py
@@ -9,8 +9,9 @@ import numpy as np
 import scipy.misc
 import os
 import matplotlib.pyplot as plt
+import scipy.signal as spy
 
-data_path = 'E:/Datafile/0716data/calcite0716.txt'  # --------------------------------------------------更改1 文件读取路径
+data_path = 'E:/Datafile/0716data/pyrite0717.txt'  # --------------------------------------------------更改1 文件读取路径
 # data为二维数组. 代码含义:把这些数据读取出来,存为numpy数组.该方法只适用于txt文件中是纯数字(float,int)
 data = np.loadtxt(data_path)
 
@@ -24,8 +25,8 @@ type(data)
 print("Maximum and minimum: " + str(data.max()), ' ', str(data.min()))
 
 # build dir for save pics
-type_dir1 = 'E:/imageset/calcite0716'      # -----------------------------------------------------------更改2 文件保存路径
-type_dir2 = 'E:/imageset/calcite0716pic/'  # ---------------------------------------更改3 文件保存路径 注意: 最后的"/"不能省
+type_dir1 = 'E:/imageset/pyrite0717'      # -----------------------------------------------------------更改2 文件保存路径
+type_dir2 = 'E:/imageset/pyrite0717pic/'  # ---------------------------------------更改3 文件保存路径 注意: 最后的"/"不能省
 if os.path.exists(type_dir1) == False:
     os.makedirs(type_dir1)
 if os.path.exists(type_dir2) == False:
@@ -43,18 +44,19 @@ for i in range(m):
     mnist数据集图片命名规则：0_00001.jpg：0表示对应图片的内容，即标签；
     00001表示标签为0的图片中第1张图片，00002为第2张图片，以此类推........
     '''
-    save_path = os.path.join(type_dir1, "0_" + str(i).zfill(5) + '.jpg')
+    save_path = os.path.join(type_dir1, "0_" + str(i).zfill(5) + '.jpg')  # -----------------------更改4 更改类即"0_"数字
     scipy.misc.imsave(save_path, matrix)
 
 # 波形图
 for i in range(m):
-    x = np.linspace(369.14, 1108.84, num=1082)
+    x = np.linspace(369.14, 1108.84, num=1082)  # ----------------------------------------------------检查txt是否需要更改
     y = data[i, :] * 10
-    plt.plot(x, y, 'k', lw=0.5)
+    yhat = spy.savgol_filter(y, 21, 7, mode='wrap')  # Savitzky-Golay filter
+    plt.plot(x, yhat, 'k', lw=0.5)
     plt.xticks([])  # 删掉刻度信息
     plt.yticks([])
     plt.axis('off')  # 删掉坐标轴
-    plt.savefig(type_dir2 + "0_" + str(i).zfill(5) + '.jpg')
+    plt.savefig(type_dir2 + "0_" + str(i).zfill(5) + '.jpg')  # -----------------------------------更改5 更改类即"0_"数字
     plt.clf()
 
 print("End\n")
