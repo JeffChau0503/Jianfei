bb8d08445e27fb3902883324e43b5dd051051d62
JeffChau0503 <Jeff_Chau0503@hotmail.com>
2018-07-17 11:55:00 +0200

    MA2

--- prep_improved.py
+++ prep_improved.py
@@ -1,11 +1,16 @@
 # encoding=utf-8
-# In[1]:
+"""
+注意！！
+检查每个生成文件夹下
+生成的第一张照片"0_00000" 要删掉，因为该照片是由 data数据第一行（波长信息）转化而来的
+"""
+
 import numpy as np
 import scipy.misc
 import os
 import matplotlib.pyplot as plt
 
-data_path = 'E:/Datafile/0716data/calcite0716.txt'  # 更改1 文件读取路径
+data_path = 'E:/Datafile/0716data/calcite0716.txt'  # --------------------------------------------------更改1 文件读取路径
 # data为二维数组. 代码含义:把这些数据读取出来,存为numpy数组.该方法只适用于txt文件中是纯数字(float,int)
 data = np.loadtxt(data_path)
 
@@ -19,16 +24,15 @@ type(data)
 print("Maximum and minimum: " + str(data.max()), ' ', str(data.min()))
 
 # build dir for save pics
-type_dir1 = 'E:/imageset/calcite0716'  # 更改2 文件保存路径
+type_dir1 = 'E:/imageset/calcite0716'      # -----------------------------------------------------------更改2 文件保存路径
+type_dir2 = 'E:/imageset/calcite0716pic/'  # ---------------------------------------更改3 文件保存路径 注意: 最后的"/"不能省
 if os.path.exists(type_dir1) == False:
     os.makedirs(type_dir1)
-type_dir2 = 'E:/imageset/calcite0716pic'
 if os.path.exists(type_dir2) == False:
     os.makedirs(type_dir2)
 
+# 灰度图
 for i in range(m):
-    # 灰度图
-    i += 1  # txt 第一行数据为wavelength数据 因此要跳过
     norm = (data[i, :] - data[i, :].min())/(data[i, :].max()-data[i, :].min())
     norm = 255 * norm
     norm = norm.astype(np.int32)
@@ -39,19 +43,18 @@ for i in range(m):
     mnist数据集图片命名规则：0_00001.jpg：0表示对应图片的内容，即标签；
     00001表示标签为0的图片中第1张图片，00002为第2张图片，以此类推........
     '''
-    save_path = os.path.join(type_dir1, "0_" + str(i-1).zfill(5) + '.jpg')  # 更改3 文件名命名
+    save_path = os.path.join(type_dir1, "0_" + str(i).zfill(5) + '.jpg')
     scipy.misc.imsave(save_path, matrix)
 
-    # 波形图
+# 波形图
+for i in range(m):
     x = np.linspace(369.14, 1108.84, num=1082)
     y = data[i, :] * 10
     plt.plot(x, y, 'k', lw=0.5)
     plt.xticks([])  # 删掉刻度信息
     plt.yticks([])
     plt.axis('off')  # 删掉坐标轴
-    save_path = os.path.join(type_dir2, "0_" + str(i - 1).zfill(5) + '.jpg')  # 更改3 文件名命名
-    plt.savefig(type_dir2, format='png', dpi=300)
+    plt.savefig(type_dir2 + "0_" + str(i).zfill(5) + '.jpg')
     plt.clf()
-
 
 print("End\n")
